dddf30effab24dea23aa81b90455b56ca23c09cf
Alex Yang <agegcn@gmail.com>
2026-06-27 00:04:06 +0800

    refactor: wrap chat content in nested divs to separate padding and max-width constraints

--- components/ChatWindow.tsx
+++ components/ChatWindow.tsx
@@ -335,7 +335,8 @@ export function ChatWindow({ session, newSessionCwd, onAgentEnd, onSessionCreate
       <>
       <div className="relative flex flex-1 overflow-hidden">
         <div ref={scrollContainerRef} className="flex-1 overflow-y-auto pt-4 [scrollbar-width:none]">
-          <div className="mx-auto max-w-[820px] px-4">
+          <div style={{ padding: `0 ${CHAT_COLUMN_PADDING}px` }}>
+            <div style={{ maxWidth: 820, margin: "0 auto" }}>
             <ExtensionStatusBar statuses={extensionStatuses} />
             <ExtensionWidgets widgets={aboveEditorWidgets} />
 
@@ -414,6 +415,7 @@ export function ChatWindow({ session, newSessionCwd, onAgentEnd, onSessionCreate
             )}
 
             <div ref={messagesEndRef} />
+            </div>
           </div>
         </div>
         <ChatMinimap
